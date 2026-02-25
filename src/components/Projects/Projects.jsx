eaccc94a538fdf743ef1785a2bb8b782f65bd1c9
AndrewKostiv <AndrewKostiv1@gmail.com>
2026-02-24 23:17:06 -0500

    updated project linking

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,18 +8,27 @@ export default function Projects() {
         <Project
           projectName={"Airsoft Tools"}
           description={"Generates random teams from a list of players"}
-          link={"https://github.com/AndrewKostiv/Airsoft"}
+          gitHubLink={"https://github.com/AndrewKostiv/Airsoft-Tools"}
+          link={"https://airsoft-tools.vercel.app"}
+          img={"./public/Airsoft Tools Image.png"}
         />
       </div>
     </div>
   );
 }
 
-export function Project({ projectName, description, link }) {
+export function Project({ projectName, description, gitHubLink, link, img }) {
   return (
     <a href={link} target="_black" className={style.Project}>
       <h3>{projectName}</h3>
       <p>{description}</p>
+      <a className={style.gitHubLink} target="_blank" href={gitHubLink}>
+        GitHub Link
+      </a>
+      <a className={style.projectLink} href={link} target="_blank">
+        <p>Project Link:</p>
+        <img src={img} alt={projectName} />
+      </a>
     </a>
   );
 }
